c4a1207f021569842c2db95373a97edaf33c4472
Zachary Voase <zacharyvoase@me.com>
2009-09-12 08:22:12 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='daterange',
+    version='0.1',
+    description='Like xrange(), but for datetime objects.',
+    author='Zachary Voase',
+    author_email='zacharyvoase@me.com',
+    url='http://bitbucket.org/zacharyvoase/daterange/',
+    packages=['daterange'],
+)
